93d55109bc8a05e4ad67d7fa7588742235f7d97e
salty-max <max@jellycat.fr>
2021-09-16 00:13:42 +0200

    Hello World :wave:

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let a = 2;
+        let b = 2;
+        assert_eq!(a + b, 4);
+    }
+}
---
+++ tests/canary.rs
@@ -0,0 +1,6 @@
+#[test]
+fn canary_test() {
+    let a = 2;
+    let b = 2;
+    assert_eq!(a + b, 4);
+}
