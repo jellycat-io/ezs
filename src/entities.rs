fa0983e1ad59a616390c10af97fb23ddfedd54fb
salty-max <max@jellycat.fr>
2021-09-16 21:19:00 +0200

    Creates an entity

--- src/entities.rs
+++ src/entities.rs
@@ -12,6 +12,12 @@ impl Entities {
     pub fn register_component<T: Any + 'static>(&mut self) {
         self.components.insert(TypeId::of::<T>(), vec![]);
     }
+
+    pub fn create_entity(&mut self) {
+        self.components
+            .iter_mut()
+            .for_each(|(_, components)| components.push(None));
+    }
 }
 
 #[cfg(test)]
@@ -23,10 +29,25 @@ mod test {
     fn register_entity() {
         let mut entities = Entities::default();
         entities.register_component::<Health>();
-        let type_id = TypeId::of::<Health>();
-        let health_components = entities.components.get(&type_id).unwrap();
+        let health_components = entities.components.get(&TypeId::of::<Health>()).unwrap();
         assert_eq!(health_components.len(), 0);
     }
 
+    #[test]
+    fn create_entity() {
+        let mut entities = Entities::default();
+        entities.register_component::<Health>();
+        entities.register_component::<Speed>();
+        entities.create_entity();
+
+        let health = entities.components.get(&TypeId::of::<Health>()).unwrap();
+        assert_eq!(health.len(), 1);
+        assert!(health[0].is_none());
+        let speed = entities.components.get(&TypeId::of::<Speed>()).unwrap();
+        assert_eq!(speed.len(), 1);
+        assert!(speed[0].is_none());
+    }
+
     struct Health(pub u32);
+    struct Speed(pub f32);
 }
